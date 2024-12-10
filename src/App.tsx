49e57530965c18e3914c53304f796ae15f8a24f4
Genevieve Coogan <gcoogan1@gmail.com>
2024-12-10 12:46:37 -0500

    Test github actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>Groupcation App!</h1>
-      <p>This is a test to main</p>
+      <p>This is test to use github actions</p>
     </div>
   );
 };
