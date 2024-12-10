51e3b7761bfc5eedacab3d7766812283946ce45f
Genevieve Coogan <gcoogan1@gmail.com>
2024-12-09 20:20:50 -0500

    Add firebase hosting for dev and prod.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: '/',
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+    server: {
+      open: mode === 'development',
+    },
+  };
 });
