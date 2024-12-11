18a9762023633f94412902acaea28392139afafb
Genevieve Coogan <gcoogan1@gmail.com>
2024-12-11 15:55:49 -0500

    Add initial authentication with firebase and redux.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useSelector } from 'react-redux';
 
-const App = () => {
+import { RootState } from './store';
+import { logout } from './services/firebase/firebaseServices';
+import useAuthListener from './features/hooks/authHook';
+import AuthForm from './testScreens/authForm';
+
+const App: React.FC = () => {
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+  useAuthListener();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Groupcation -- Coming Soon!!</h1>
+      {user ? (
+        <div>
+          <h1>Welcome, {user.email}</h1>
+          <button onClick={() => logout()}>Sign Out</button>
+        </div>
+      ) : (
+        <div>
+          <AuthForm mode="signup" />
+          <AuthForm mode="login" />
+        </div>
+      )}
     </div>
   );
 };
