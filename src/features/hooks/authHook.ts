402c548647e74436dac0286062776e68ea2882d9
Genevieve Coogan <gcoogan1@gmail.com>
2024-12-11 15:55:49 -0500

    Add initial authentication with firebase and redux.

---
+++ src/features/hooks/authHook.ts
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from 'store';
+
+import { onAuthStateChangedListener } from '../../services/firebase/firebaseServices';
+import { setUser, setLoading } from '../../features/stateSlices/authSlice';
+
+/* Listens for authentication state changes and updates the Redux store accordingly, 
+then removes listener (cleanup) */
+const useAuthListener = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    // Set loading state to true before setting up the listener
+    dispatch(setLoading(true));
+
+    // Set up the Firebase auth state listener
+    const authStateListener = onAuthStateChangedListener((currentUser) => {
+      // Update Redux store with the current user or null
+      dispatch(setUser(currentUser));
+      // Set loading state to false after fetching user info
+      dispatch(setLoading(false));
+    });
+
+    // Cleanup: authListener the listener when the component unmounts
+    return () => {
+      if (typeof authStateListener === 'function') {
+        authStateListener();
+      }
+    };
+  }, [dispatch]); // Ensures the effect only runs when `dispatch` changes
+};
+
+export default useAuthListener;
