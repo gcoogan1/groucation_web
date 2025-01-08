f6ad148a1fdd5301a86d8a10414fbcbe2b94cd23
Genevieve Coogan <gcoogan1@gmail.com>
2025-01-07 20:53:32 -0500

    Add vite-plugin-svgr package and config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     base: '/',
-    plugins: [react()],
+    plugins: [
+      react(),
+      svgr(),
+    ],
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
