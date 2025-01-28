a5c708c2da8a98d8d787b3b644db3322a66df598
Genevieve Coogan <gcoogan1@gmail.com>
2025-01-28 13:53:23 -0500

    Add router logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import { useSelector } from 'react-redux';
+import { RouterProvider } from "react-router-dom";
 
 import { designTokens } from './styles/designTokens';
 import Add from './assets/Icons/Add.svg?react'; // special import for svgs with vite-plugin-svgr
@@ -7,18 +8,22 @@ import Button from './components/Button/Button';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './screens/Homepage/Homepage';
 
-// import { RootState } from './store';
-// import useAuthListener from './features/hooks/authHook';
+import { RootState } from './store';
+import useAuthListener from './features/hooks/authHook';
 // import AuthForm from './testScreens/AuthForm';
 // import HomeScreen from './testScreens/HomeScreen';
-// import useUserDetails from './features/hooks/userDetailsHook';
+import useUserDetails from './features/hooks/userDetailsHook';
+import { useSelector } from 'react-redux';
+import { router } from "./routes/router";
 
 const App: React.FC = () => {
-  // useAuthListener();
+  useAuthListener();
 
-  // const { user, loading } = useSelector((state: RootState) => state.auth);
+  const { user, loading } = useSelector((state: RootState) => state.auth);
 
-  // useUserDetails(user);
+  useUserDetails(user);
+
+  // console.log("User", user)
 
   // if (loading) {
   //   return <div>Loading...</div>;
@@ -34,7 +39,7 @@ const App: React.FC = () => {
           <AuthForm mode="login" />
         </div>
       )} */}
-      <Homepage />
+      <RouterProvider router={router} />
     </Layout>
   );
 };
---
+++ src/routes/router.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import { ROUTES } from "./routes";
+
+interface AuthRouteProps {
+  element: React.ReactNode;
+}
+
+const AuthenticatedRoute: React.FC<AuthRouteProps> = ({ element }) => {
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // If no user is found, redirect to login/signup page
+  if (!user) {
+    return <Navigate to="/signup" />;
+  }
+
+  return element;
+};
+
+const UnauthenticatedRoute: React.FC<AuthRouteProps> = ({ element }) => {
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // If user is authenticated, redirect to home or any other route
+  if (user) {
+    return <Navigate to="/" />;
+  }
+
+  return element;
+};
+
+
+const routesWithAuth = ROUTES.map((route) => {
+  // Check if the route needs to be protected
+  if (route.isAuth) {
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        element={<AuthenticatedRoute element={route.element} />}
+      />
+    );
+  }
+
+  // If the route does not require authentication, wrap it in UnauthenticatedRoute
+  return (
+    <Route
+      key={route.path}
+      path={route.path}
+      element={<UnauthenticatedRoute element={route.element} />}
+    />
+  );
+});
+
+export const router = createBrowserRouter(createRoutesFromElements(routesWithAuth));
+
